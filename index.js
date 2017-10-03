206d8ba0804a341ec597e684fef47b96a9422c40
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-10-03 00:19:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello, World")// write your code below!
 console.log("This is my first line of code")
-var dog;
+var dog = "thirsty";
 if (dog=== "hungry")
 {
   console.log("refill the food bowl")
